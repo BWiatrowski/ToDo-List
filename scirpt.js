140b2f2d7095f9ea58228440d55c6a5b16a2eb6c
Blazej Wiatrowski <blazej.wiatrowski96@gmail.com>
2022-12-12 20:43:09 +0100

    Implement ToDo List

---
+++ scirpt.js
@@ -0,0 +1,133 @@
+let todoInput //miejsce, gdzie uzytkownik wpisuje tresc zadania
+let errorInfo // info o braku zadan / koniecznosci wpisania tekstu
+let addBtn // orzycisk ADD - dodaje nowe elementy do listy
+let ulList // lista zadan, tagi UL
+let newTodo // nowo dodany LI, nowe zadanie
+
+let popup // popup
+let popupInfo // tekst w popupie, jak się doda pusty tekst
+let todoToEdit // edytowany Todo
+let popupInput // input w popupie
+let popupAddBtn // przycisk "zatwierdź" w popupie
+let popupCloseBtn // przycisk "anuluj" w popupie
+
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	//pobieramy wszystkie elementy
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	//nadajemy nasluchiwanie
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		createToolsArea()
+		ulList.append(newTodo)
+
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Enter your text!'
+	}
+}
+
+// 1. tworzy nowy element (li)
+// 2. dodaje nowy element do ul listy
+// 3. funkcja odpalana na click w przycisk ADD
+// 4. przechwytuje tresc z inputa i umieszcza go w nowo utworzonym LI
+// 5. funkcja nie doda do listy pustego 'todosa'
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = e => {
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
+	popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = ''
+}
+
+const changeTodoText = () => {
+	if(popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'You must enter any content!'
+	}
+}
+
+const deleteTodo = e => {
+	e.target.closest('li').remove()
+
+	const allTodos = ulList.querySelectorAll('li')
+
+	if(allTodos.length === 0) {
+		errorInfo.textContent = 'No tasks.'
+	}
+}
+
+const enterKeyCheck = e => {
+	if(e.key === 'Enter') {
+		addNewTodo()
+	}
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+
